a7d5c0ba16fa0850b308b224211147fbd55ba398
Tahir Usman <tahirusmanflancer@gmail.com>
2024-06-23 17:09:17 +0500

    push my number guessing game to github

---
+++ README.md
@@ -0,0 +1,4 @@
+# tahir-number-guessing-game
+This is a cli-number-guessing -game
+fyggiul
+"# 01--Number-Guessing-Game" 
---
+++ main.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+console.log();
+console.log("Welcome To The Guessing Numbers Game!");
+console.log();
+console.log("I Have Picked a Number Between 1 and 6. Try to guess it!");
+console.log();
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        message: "Please Guessing Your Number",
+        type: "number",
+        name: "userGuessNumber",
+    },
+]);
+if (answers.userGuessNumber > 0 && answers.userGuessNumber < 7) {
+    if (answers.userGuessNumber === randomNumber) {
+        console.log("Congrats! The Number You Guessed Was Correct.");
+    }
+    else {
+        console.log("You Guessed The Wrong Number!");
+    }
+}
+else {
+    console.log("The Range of Your Guessing Number is Not Met!");
+}
